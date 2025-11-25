424603b5875dbaecf7961e5b83da060cfd127fb7
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2025-11-25 23:27:26 +0500

    portfolio

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,7 +6,15 @@ function Hero() {
     <div className="hero" >
       <div className="overlay"></div> {/* qora yarim shaffof qatlam */}
       <div className="hero__content">
-        <h1>Hello, I'm Sanjarbek 👋</h1>
+        <h1>
+          <span>Hello,</span>
+          <span> </span>
+          <span>I'm</span>
+          <span> </span>
+          <span>Sanjarbek</span>
+          <span> </span>
+          <span>👋</span>
+          </h1>
         <p>
           Frontend Developer | Creator of modern and responsive web applications
         </p>
