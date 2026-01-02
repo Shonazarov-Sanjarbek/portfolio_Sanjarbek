07b3f9e19a6bb3ab10128ff57b7eb42fb789fdda
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2026-01-02 21:00:02 +0500

    portfolio

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import logo from "../../assets/San_Dev.png";
+import { BsGithub, BsInstagram, BsTelegram } from "react-icons/bs";
 
 function Header() {
   const [open, setOpen] = useState(false);
@@ -23,8 +24,10 @@ function Header() {
     <header className="header">
       <div className="container">
         <div className="header__inner">
+          {/* LOGO */}
           <img src={logo} alt="logo" className="header__logo" />
 
+          {/* MARKAZIY MENU */}
           <ul className="header__menu">
             {menuItems.map((item) => (
               <li
@@ -37,6 +40,42 @@ function Header() {
             ))}
           </ul>
 
+          {/* 🔥 O‘NG TOMON: ICON + RAQAM */}
+          <div className="header__contact">
+            <div>
+              {/* 🔢 RAQAM */}
+              <a href="tel:+998970058747">+99897 005 87 47</a>
+              {/* 🔗 ICONKALAR */}
+              <div className="header__right-socials">
+                <a
+                  href="https://t.me/bek_8747_uzb"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="Telegram"
+                >
+                  <BsTelegram />
+                </a>
+                <a
+                  href="https://instagram.com/sanjarbek_8747"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="Instagram"
+                >
+                  <BsInstagram />
+                </a>
+                <a
+                  href="https://github.com/Shonazarov-Sanjarbek"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="GitHub"
+                >
+                  <BsGithub />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* BURGER */}
           <button
             className="header__burger"
             onClick={() => setOpen(!open)}
@@ -49,6 +88,7 @@ function Header() {
         </div>
       </div>
 
+      {/* SIDEBAR O‘ZGARMADI */}
       <div className={`sidebar ${open ? "open" : ""}`}>
         <button className="sidebar__close" onClick={() => setOpen(false)}>
           &times;
